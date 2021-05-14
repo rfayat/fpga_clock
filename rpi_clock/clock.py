0c76e9bcf78554e8099df91acbbe97c12299a9e1
Romain Fayat <r.fayat@gmail.com>
2021-05-14 13:11:15 +0200

    Add enable pin on the rpi

--- rpi_clock/clock.py
+++ rpi_clock/clock.py
@@ -4,50 +4,85 @@ Author: Romain Fayat, May 2021
 """
 import RPi.GPIO as GPIO
 import time
+from functools import wraps
 
-# Default output pin for the raspberry
-OUTPUT_PIN = 21  # BCM numbering
+# Default pins for the raspberry (BCM numbering)
+ENABLE_PIN = 21
+RESET_PIN = 20
 
 
 class Clock_Handler():
     "Object handling the communication with the FPGA"
 
-    def __init__(self, output_pin=OUTPUT_PIN):
+    def add_verbose_message(message=""):
+        "Method decorator for adding a message if self.verbose is True"
+        def decorator(f):
+            @wraps(f)
+            def decorated(self, *args, **kwargs):
+                if self.verbose:
+                    print(message)
+                return f(self, *args, **kwargs)
+            return decorated
+        return decorator
+
+    def __init__(self, enable_pin=ENABLE_PIN, reset_pin=RESET_PIN,
+                 verbose=False):
         "Instantiate the object and take care of GPIO setup"
-        self.output_pin = output_pin
+        self.verbose = verbose
+        self.enable_pin = enable_pin
+        self.reset_pin = reset_pin
         self.setup_gpio()
 
+    @add_verbose_message("Setting up pins")
     def setup_gpio(self):
         "Setup of the gpio pins"
         GPIO.setwarnings(False)
         # Use BCM pin numbering
         GPIO.setmode(GPIO.BCM)
-        # Set the output pin as an output on low
-        GPIO.setup(self.output_pin, GPIO.OUT)
-        GPIO.output(self.output_pin, GPIO.LOW)
+        # Set the enable pin as an output on low
+        GPIO.setup(self.enable_pin, GPIO.OUT)
+        GPIO.output(self.enable_pin, GPIO.LOW)
+        # Set the reset pin as an output on low after a brief pulse
+        GPIO.setup(self.reset_pin, GPIO.OUT)
+        self.reset()
+
+    @add_verbose_message("Resetting pins")
+    def reset(self, reset_pulse_duration=.1):
+        "Send a pulse of reset_pulse_duration seconds of on the reset pin"
+        GPIO.output(self.reset_pin, GPIO.HIGH)
+        time.sleep(reset_pulse_duration)
+        GPIO.output(self.reset_pin, GPIO.LOW)
 
     @property
-    def output_state(self):
-        "Return the state of the output (False for low, True for high)"
-        return GPIO.input(self.output_pin)
+    def enable_state(self):
+        "Return the state of the enable pin (False for low, True for high)"
+        return GPIO.input(self.enable_pin)
 
-    def toggle_output(self):
-        "Change the state of the output pin (e.g. low if it was high)"
-        GPIO.output(self.output_pin, not self.output_state)
+    @add_verbose_message("Setting enable pin to high")
+    def enable(self):
+        "Change the state of the enable pin to high"
+        GPIO.output(self.enable_pin, GPIO.HIGH)
 
+    @add_verbose_message("Setting enable pin to low")
+    def disable(self):
+        "Change the state of the enable pin to low"
+        GPIO.output(self.enable_pin, GPIO.LOW)
+
+    @add_verbose_message("Cleaning up")
     def cleanup(self):
         "Clean up the GPIOs"
-        GPIO.output(self.output_pin, GPIO.HIGH)
+        self.disable()
+        self.reset()
         GPIO.cleanup()
 
 if __name__ == "__main__":
-    handler = Clock_Handler()
-    try:
-        while True:
-            time.sleep(3.)
-            status_str = "high" if handler.output_state else "low"
-            status_str_new = "low" if handler.output_state else "high"
-            print(f"Was {status_str}, setting to {status_str_new}")
-            handler.toggle_output()
-    finally:
-        handler.cleanup()
+    handler = Clock_Handler(verbose=True)
+    time.sleep(1.)
+
+    for _ in range(4):
+        handler.enable()
+        time.sleep(3.)
+        handler.disable()
+        time.sleep(1.)
+    time.sleep(2.)
+    handler.cleanup()
