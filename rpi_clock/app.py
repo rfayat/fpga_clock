e95330ba13b85ae7bd972c73387ec9c6a8dbb066
Romain Fayat <r.fayat@gmail.com>
2021-05-14 17:33:05 +0200

    Add remote control of the FPGA by the RPI

--- rpi_clock/app.py
+++ rpi_clock/app.py
@@ -7,23 +7,29 @@ from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from typing import Optional
 from pathlib import Path
-# from .clock import Clock_Handler
+from .clock import Clock_Handler
 
 # Initialize the app
 app = FastAPI()
 templates = Jinja2Templates(directory="rpi_clock/templates/")
+clock_handler = Clock_Handler(verbose=True)
 
 @app.get("/")
 async def home(request: Request):
     "Home page for starting and stopping the clock"
-    return templates.TemplateResponse("menu.html", context={"request": request})
+    enable_state = clock_handler.enable_state
+    context = {"request": request, "enable_state": enable_state}
+    return templates.TemplateResponse("menu.html", context=context)
 
 @app.get("/start")
 async def start():
     "Start the fpga clock"
+    clock_handler.enable()
     return RedirectResponse("/")
 
 @app.get("/stop")
 async def stop():
     "Stop the fpga clock"
+    clock_handler.disable()
+    clock_handler.reset()
     return RedirectResponse("/")
--- rpi_clock/main.sh
+++ rpi_clock/main.sh
@@ -1,3 +1,2 @@
 # Launch the app
-firefox http://127.0.0.1:5000/ &
 uvicorn rpi_clock.app:app --reload --port 5000 --host 127.0.0.1;
