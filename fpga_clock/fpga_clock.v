d3afce19912f119de1ac6e900cac1fe319945587
rfayat <r.fayat@gmail.com>
2021-05-17 19:21:04 +0200

    Update FPGA code to add a LED blinking indicating the enable status

--- fpga_clock/fpga_clock.v
+++ fpga_clock/fpga_clock.v
@@ -1,11 +1,20 @@
 module fpga_clock(
   input clk ,
-  input enable,
-  input reset ,
-  output syncout);
+  input enable, // pin 87 (RPI GPIO 23)
+  input reset,  // pin 89 (RPI GPIO 24)
+  output syncout,  // pin 73
+  output is_on, // pin 96
+  output is_enabled // pin 93
+  );
 
-  parameter FREQ_CLK = 2000000;
+  parameter FREQ_CLK = 25000000;
   wire pulsePeriodique ; // True for one clk period every FREQ_CLK clk posedges
+
+  // LED on pin 81 indicating that the FPGA is on
+  assign is_on = 1;
+
+  // LED on pin
+  assign is_enabled = enable;
 
   // Create the frequency diviser
   Frequency_Diviser #(.FREQ_CLK(FREQ_CLK)) diviser(
